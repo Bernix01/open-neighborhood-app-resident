182feae50248eda6933c135aaf78785b2d0a5c48
Guillermo Bernal <gbernal096@gmail.com>
2020-07-15 22:59:36 -0500

    android and ios caps

---
+++ ios/App/public/precache-manifest.aecb64e5b0171632d2deadc7195ae7f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352c0550b8801debd5eed20dcd2ebd60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c71362fee4074de0011",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5a4723259cecda6e9991",
+    "url": "/static/js/2.38f27930.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.38f27930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c71362fee4074de0011",
+    "url": "/static/js/main.28e82e83.chunk.js"
+  },
+  {
+    "revision": "aa59354c6245e307e8ac",
+    "url": "/static/js/runtime-main.9725dad0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
